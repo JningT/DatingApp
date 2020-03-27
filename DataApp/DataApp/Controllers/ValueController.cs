bc6299a761bf6a271ce2f6ba89e3e32f5da5ba59
nningsaki@gmail.com <nningsaki@gmail.com>
2020-03-27 12:00:54 -0400

    initial commit

---
+++ DataApp/DataApp/Controllers/ValueController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValueController(DataContext  context)
+        {
+            _context = context;
+        }
+        // GET: api/Value
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // GET: api/Value/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var values = await _context.Values.FirstOrDefaultAsync(x => x.Id  == id);
+            return Ok(values);
+        }
+
+        // POST: api/Value
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Value/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
